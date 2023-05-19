10c6cd1f659c204e8cf5a4ebf828ef68ea186eba
Remi-Mergen <remi.mergen@epitech.eu>
2023-05-19 18:06:45 +0200

    feat: init Cook

--- include/kitchen/Cook.hpp
+++ include/kitchen/Cook.hpp
@@ -6,13 +6,28 @@
 */
 #pragma once
 
+#include <iostream>
+#include <memory>
 #include "Thread.hpp"
+#include "IPizza.hpp"
 
 class Cook {
-	public:
-		Cook();
-		~Cook() = default;
+    public:
+        Cook(const std::shared_ptr<IPizza> &pizza, int bakeTime = 1) : _pizza(pizza), _thread([=, this] {
+            this->cook(bakeTime);
+            this->stop();
+        }) {};
+        ~Cook() = default;
 
-	private:
+        void cook(int time = 1, int multiplier = 1) {
+            //TODO: log
+            std::this_thread::sleep_for(std::chrono::seconds (time * multiplier));
+        }
+        void stop() {
+            //TODO: log
+            _thread.detach();
+        }
+    private:
+        std::shared_ptr<IPizza> _pizza;
         Thread _thread;
 };
