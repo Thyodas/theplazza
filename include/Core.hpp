aa7c5ca0f8e5d300d0f0a8a07b522a8ff858e30d
Remi-Mergen <remi.mergen@epitech.eu>
2023-05-19 17:26:16 +0200

    feat: init Core

--- include/Core.hpp
+++ include/Core.hpp
@@ -5,14 +5,26 @@
 ** Core.hpp
 */
 #pragma once
+
+#include <queue>
 #include "Config.hpp"
+#include "Shell.hpp"
 
 class Core {
     public:
-        Core();
+        Core(Config conf);
         ~Core() = default;
         void getArgs(Config conf);
-    protected:
+        void mainLoop();
+        void fillCommands();
+        void sendCommands();
+        void createKitchen();
+
     private:
-        Config conf;
+        Config _conf;
+        Shell shell;
+        std::queue<command_t> _commands;
+        float _multiplier;
+        int _nbCooks;
+        double _refillTime;
 };
--- source/Core.cpp
+++ source/Core.cpp
@@ -1,5 +1,34 @@
-//
-// Created by remim on 5/15/2023.
-//
+/*
+** EPITECH PROJECT, 2023
+** plazza
+** File description:
+** Core.cpp
+*/
 
 #include "Core.hpp"
+
+Core::Core(Config conf) : _conf(conf), shell() {}
+
+void Core::getArgs(Config conf)
+{
+    _multiplier = conf.getTimeMultiplier();
+    _nbCooks = conf.getNbCooksPerKitchen();
+    _refillTime = conf.getRefillIngredient();
+}
+
+void Core::mainLoop()
+{
+    while (true) {
+        fillCommands();
+        sendCommands();
+    }
+}
+
+void Core::fillCommands()
+{
+    shell.getInput();
+    shell.extractCommand();
+    std::vector<command_t> commands = shell.getCommands();
+    for (auto &command : commands)
+        _commands.push(command);
+}
